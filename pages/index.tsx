2966559f3970267a8c07dc7883447d25c0257f93
Phil Price <philprice@gmail.com>
2020-06-28 13:24:06 -0700

    Tweak heading colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react";
 import { getGalleryIndex } from "@/gallery";
-import { Box, GridList, GridListTile, useMediaQuery, Link } from "@material-ui/core";
+import { Box, GridList, GridListTile, useMediaQuery, Link, Typography } from "@material-ui/core";
 import theme from "@/components/Theme";
 
 function Home() {
@@ -9,17 +9,20 @@ function Home() {
 
   return (
     <Box>
-      <GridList cellHeight={300} cols={isDesktop ? 3 : 1} spacing={10}>
-        {items.map((p) => (
-          <GridListTile key={p.name} color="white">
-            <a href={p.url}>
-              <Box display="flex" border={1} color="#EEEEEE" height={290} overflow="hidden">
-                <img src={p.img} width="100%" />
-              </Box>
-            </a>
-          </GridListTile>
-        ))}
-      </GridList>
+      <Typography variant="h2">Gallery</Typography>
+      <Box marginTop={2}>
+        <GridList cellHeight={300} cols={isDesktop ? 3 : 1} spacing={10}>
+          {items.map((p) => (
+            <GridListTile key={p.name} color="white">
+              <a href={p.url}>
+                <Box display="flex" border={1} color="#EEEEEE" height={290} overflow="hidden">
+                  <img src={p.img} width="100%" />
+                </Box>
+              </a>
+            </GridListTile>
+          ))}
+        </GridList>
+      </Box>
     </Box>
   );
 }
