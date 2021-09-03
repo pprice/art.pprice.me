702bc5203c99f339f36c534b289eb456bee0a582
Phil Price <philprice@gmail.com>
2021-09-03 13:27:51 -0700

    Fix typo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -64,7 +64,7 @@ MyDocument.getInitialProps = async (ctx) => {
   ctx.renderPage = () =>
     originalRenderPage({
       // eslint-disable-next-line react/display-name
-      enhanceApp: (App) => (props) => sheets.caollect(<App {...props} />),
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
 
   const initialProps = await Document.getInitialProps(ctx);
